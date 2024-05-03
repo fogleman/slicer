e07cf4b765634eecb0345db73255b0795b142376
Michael Fogleman <fogleman@gmail.com>
2024-05-03 10:43:35 -0400

    add subdivisions option

--- cmd/slicer/main.go
+++ cmd/slicer/main.go
@@ -19,13 +19,14 @@ import (
 )
 
 var (
-	quiet     = kingpin.Flag("quiet", "Run in silent mode.").Short('q').Bool()
-	directory = kingpin.Flag("directory", "Set the output directory.").Short('d').ExistingDir()
-	size      = kingpin.Flag("size", "Set the slice thickness.").Required().Short('s').Float()
-	width     = kingpin.Flag("width", "Set the raster width in model units.").Required().Short('w').Float()
-	height    = kingpin.Flag("height", "Set the raster height in model units.").Required().Short('h').Float()
-	scale     = kingpin.Flag("scale", "Set the raster scale.").Short('x').Required().Float()
-	files     = kingpin.Arg("files", "Mesh files to slice.").Required().ExistingFiles()
+	quiet        = kingpin.Flag("quiet", "Run in silent mode.").Short('q').Bool()
+	directory    = kingpin.Flag("directory", "Set the output directory.").Short('d').ExistingDir()
+	size         = kingpin.Flag("size", "Set the slice thickness.").Required().Short('s').Float()
+	subdivisions = kingpin.Flag("subdivisions", "Set the number of slice subdivisions.").Default("1").Short('b').Int()
+	width        = kingpin.Flag("width", "Set the raster width in model units.").Required().Short('w').Float()
+	height       = kingpin.Flag("height", "Set the raster height in model units.").Required().Short('h').Float()
+	scale        = kingpin.Flag("scale", "Set the raster scale.").Short('x').Required().Float()
+	files        = kingpin.Arg("files", "Mesh files to slice.").Required().ExistingFiles()
 )
 
 func main() {
@@ -68,7 +69,8 @@ func process(infile string) {
 
 	// slice mesh
 	done = timed("slicing mesh")
-	layers := slicer.SliceMesh(mesh, *size)
+	step := *size / float64(*subdivisions)
+	layers := slicer.SliceMesh(mesh, step)
 	done()
 
 	// determine output dir
@@ -86,21 +88,24 @@ func process(infile string) {
 }
 
 type job struct {
-	i     int
-	layer slicer.Layer
-	box   fauxgl.Box
+	i      int
+	layers []slicer.Layer
+	box    fauxgl.Box
 }
 
 func render(box fauxgl.Box, layers []slicer.Layer, dir string) {
+	b := *subdivisions
 	wn := runtime.NumCPU()
-	ch := make(chan job, len(layers))
+	ch := make(chan job, len(layers)/b)
 	var wg sync.WaitGroup
 	for wi := 0; wi < wn; wi++ {
 		wg.Add(1)
 		go worker(ch, &wg, dir)
 	}
-	for i, l := range layers {
-		ch <- job{i, l, box}
+	for i := 0; i < len(layers)/b; i++ {
+		i0 := i * b
+		i1 := i0 + b
+		ch <- job{i, layers[i0:i1], box}
 	}
 	close(ch)
 	wg.Wait()
@@ -118,6 +123,21 @@ func fastRGBAToGray(src *image.RGBA) *image.Gray {
 			i += 4
 			j++
 		}
+	}
+	return dst
+}
+
+func averageImages(images []*image.Gray) *image.Gray {
+	if len(images) == 1 {
+		return images[0]
+	}
+	dst := image.NewGray(images[0].Bounds())
+	for i := range dst.Pix {
+		var sum int
+		for _, im := range images {
+			sum += int(im.Pix[i])
+		}
+		dst.Pix[i] = uint8(sum / len(images))
 	}
 	return dst
 }
@@ -140,28 +160,34 @@ func worker(ch chan job, wg *sync.WaitGroup, dir string) {
 	h := *height * s
 	for j := range ch {
 		i := j.i
-		layer := j.layer
+		layers := j.layers
 		box := j.box
 		center := box.Center()
 		dc := gg.NewContext(int(w), int(h))
 		dc.InvertY()
-		dc.SetRGB(1, 1, 1)
-		dc.Clear()
 		dc.Translate(w/2, h/2)
 		dc.Scale(s, s)
 		dc.Translate(-center.X, -center.Y)
 		dc.SetFillRuleWinding()
-		for _, path := range layer.Paths {
-			dc.NewSubPath()
-			for _, point := range path {
-				dc.LineTo(point.X, point.Y)
+
+		var images []*image.Gray
+		for _, layer := range layers {
+			dc.SetRGB(0, 0, 0)
+			dc.Clear()
+			for _, path := range layer.Paths {
+				dc.NewSubPath()
+				for _, point := range path {
+					dc.LineTo(point.X, point.Y)
+				}
+				dc.ClosePath()
 			}
-			dc.ClosePath()
+			dc.SetRGB(1, 1, 1)
+			dc.Fill()
+			im := fastRGBAToGray(dc.Image().(*image.RGBA))
+			images = append(images, im)
 		}
-		dc.SetRGB(0, 0, 0)
-		dc.Fill()
-		src := dc.Image()
-		dst := fastRGBAToGray(src.(*image.RGBA))
+		dst := averageImages(images)
+
 		path, _ := filepath.Abs(filepath.Join(dir, fmt.Sprintf("%04d.png", i)))
 		savePNG(path, dst)
 		fmt.Println(path)
